76fab96f4687e844db8b536f4d7f446e4b561636
123kamatcho <93836498+123kamatcho@users.noreply.github.com>
2026-01-05 10:00:00 +0200

    chore: initial project configuration and dependencies

--- android/app/src/main/kotlin/com/example/saboura/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.saboura
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/networking/api_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-
-class ApiService {
-  final Dio _dio;
-
-  ApiService(this._dio);
-
-  // Add methods for API calls if needed, though for now we use Firebase
-}
