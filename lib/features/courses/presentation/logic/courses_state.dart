d6e26ec458f34fce103d9501b640da9a1fabf507
123kamatcho <93836498+123kamatcho@users.noreply.github.com>
2026-02-11 04:00:00 +0200

    feat: develop courses cubit for data fetching

---
+++ lib/features/courses/presentation/logic/courses_state.dart
@@ -0,0 +1,67 @@
+import '../../domain/entities/course_entity.dart';
+import '../../domain/entities/quiz_result_entity.dart';
+import '../../domain/entities/leaderboard_entity.dart';
+import '../../domain/entities/notification_entity.dart';
+
+abstract class CoursesState {
+  const CoursesState();
+}
+
+class CoursesInitial extends CoursesState {
+  const CoursesInitial();
+}
+
+class CoursesLoading extends CoursesState {
+  const CoursesLoading();
+}
+
+class CoursesLoaded extends CoursesState {
+  final List<CourseEntity> courses;
+  const CoursesLoaded(this.courses);
+}
+
+class TeacherCoursesLoaded extends CoursesState {
+  final List<CourseEntity> courses;
+  const TeacherCoursesLoaded(this.courses);
+}
+
+class QuizResultsLoaded extends CoursesState {
+  final List<QuizResultEntity> results;
+  const QuizResultsLoaded(this.results);
+}
+
+class LeaderboardLoaded extends CoursesState {
+  final List<LeaderboardEntity> leaderboard;
+  const LeaderboardLoaded(this.leaderboard);
+}
+
+class NotificationsLoaded extends CoursesState {
+  final List<NotificationEntity> notifications;
+  const NotificationsLoaded(this.notifications);
+}
+
+class LessonStatusUpdated extends CoursesState {
+  const LessonStatusUpdated();
+}
+
+class CourseAddedSuccess extends CoursesState {
+  const CourseAddedSuccess();
+}
+
+class EnrollmentSuccess extends CoursesState {
+  const EnrollmentSuccess();
+}
+
+class QuizResultSavedSuccess extends CoursesState {
+  const QuizResultSavedSuccess();
+}
+
+class FileUploadedSuccess extends CoursesState {
+  final String url;
+  const FileUploadedSuccess(this.url);
+}
+
+class CoursesError extends CoursesState {
+  final String error;
+  const CoursesError(this.error);
+}
